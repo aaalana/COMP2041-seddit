ef9daaea714d2c253c8ac900b0e5c8975954c1bd
Alana Hua <z5209503@ad.unsw.edu.au>
2019-08-04 23:46:13 +1000

    css added again

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -10,7 +10,7 @@
 // this will allow us to verify your apps behaviour with 
 // different datasets.
 function initApp(apiUrl) {
-     // your app initialisation goes here
+    // your app initialisation goes here
     
     // MAKING THE BASIC WEBPAGE SKELETON 
     
@@ -74,7 +74,7 @@ function initApp(apiUrl) {
     document.getElementById('root').appendChild(header);
     
     // MAKING THE FEED //
-    // CREATING THE ELEMENTS OF FEED
+    // CREATING THE ELEMENTS OF POST - making a post template
     const main = document.createElement('main');
     main.role = 'main';
     
@@ -114,7 +114,7 @@ function initApp(apiUrl) {
     feedTitle.textContent = `Avenger’s Endgame Officially Passes Avatar To
                              Become The Highest Grossing Movie Of All Time`;
                      
-    const feedPara = document.createElement('p');
+    const feedPara = document.createElement('span');
     feedPara.className = 'post-author'; 
     const authorAttribute = document.createAttribute('data-id-author');
     feedPara.setAttributeNode(authorAttribute);              
@@ -122,17 +122,19 @@ function initApp(apiUrl) {
     
     // other...
     // images not added
-    const feedDate = document.createElement('p');
+    const feedDate = document.createElement('span');
     feedDate.textContent = '4/08/2019';
     
     const feedDescript = document.createElement('p');
     feedDescript.textContent = 'description: text 7';
     
-    const feedComments = document.createElement('div');
+    const feedComments = document.createElement('span');
     feedComments.textContent = '0 comments';
+    feedComments.className = 'feed-comments';
     
-    const feedSubseddit = document.createElement('p');
-    feedComments.textContent = '/s/meme';
+    const feedSubseddit = document.createElement('span');
+    feedSubseddit.textContent = '/s/meme';
+    feedSubseddit.className = 'feed-subseddit';
     
     // APPENDING ELEMENTS TO HEADER
     feedHeader.appendChild(popularPosts);
@@ -204,15 +206,20 @@ function initApp(apiUrl) {
     document.getElementById('root').appendChild(loginDiv);
     
     // buttons functionality for login
+    
+    // open the login form and close the sign up form (if open) when
+    // the login button is clicked
     headerButtonL.onclick = () => {
         document.getElementById('login').style.display = 'block';
         document.getElementById('sign-up').style.display = 'none';
     }
  
+    // log the user in
     loginSubmit.onclick = () => {
         alert("Login has failed! :(");
     }
     
+    // close the login form when the close button is clicked
     loginClose.onclick = () => {
         document.getElementById('login').style.display = 'none';
     }
@@ -238,18 +245,25 @@ function initApp(apiUrl) {
     document.getElementById('root').appendChild(signDiv);
  
     // buttons functionality for sign up
+    
+    // open the sign up form and close the login form (if open) when
+    // the sign up button is clicked
     headerButtonS.onclick = () => {
         document.getElementById('sign-up').style.display = 'block';
         document.getElementById('login').style.display = 'none';
     }
    
-    signSubmit.onclick = () => {
-        // basic input validation 
-        const illegalChars = /\W/;
-        
+    // submit user details into user data base when the submit button 
+    // is clicked
+    // note: sign up is failing for subset 0
+    fetch('./data/feed.json').then(r=>r.text()).then(r => console.log(r))
+     signSubmit.onclick = () => {
         let username = document.getElementById('sign-username').value;
         let password = document.getElementById('sign-password').value;
+        
       
+        // opens the user database and finds if the username signed up
+        // exists in the user database
         let user = '';
         let url = "../data/users.json";
         const request = new XMLHttpRequest();
@@ -263,24 +277,36 @@ function initApp(apiUrl) {
           });
         }
 
+        // basic input validation 
+        
+        // 'require' attribute is already in sign up fields so that 
+        // usernames and passwords must contain at least one character
+             
+        // usernames must contain letters and numbers
+        //console.log(user);
+        const legalChars = /\w/;
+        if (!username.match(legalChars) || !password.match(legalChars)) {
+	        let error = "Error: The username/password contains illegal characters.";
+		    alert(error);
+        // usernames must be available and not found in the database
+        } else if (typeof user !== 'undefined') {
+            let error = "Error: This username has been taken.";
+		    alert(error);
+        } else {
+            console.log(user);
+            console.log(user.username);
+            alert("Error: Sign up has failed! :(");
+        }
+      
         /*fetch('../data/users.json')
         	.then(res => res.json())
 	        .then(json => {
 	            console.log("yes");
-	            //let sameUsername = json.filter(user => user.username === username);
+	          
 	        })*/
-	        
-        if (illegalChars.test(username) || illegalChars.test(password)) {
-		    let error = "The username/password contains illegal characters.\n";
-		    alert(error);
-        } else if (user.username !== '') {
-            let error = "This username has been taken.\n";
-		    alert(error);
-        } else {
-            alert("Sign up has failed! :(");
-        }
     }
     
+    // close the sign up form when the close button is clicked
     signClose.onclick = () => {
         document.getElementById('sign-up').style.display = 'none';
     }
@@ -293,27 +319,33 @@ function initApp(apiUrl) {
         request2.responseType = 'json';
         request2.send();
         request2.onload = function() {
+            
             // sorting posts from most recent to least
             let postData = request2.response.posts;
             let sortedPosts = postData.sort(function(a, b){
                 return b.meta.published-a.meta.published
             });
             
-            console.log(sortedPosts);
+            // console.log(sortedPosts);
+            
             // adding the post to the feed
             for(let post of sortedPosts) {
+                // clone the template post element and modify details for 
+                // each post
                 let feedPost = feedLi.cloneNode(true);
-                console.log(feedPost);
+                
+                // use console.log to check which childNode should be 
+                // picked to get a specific element of the post
+                // console.log(feedPost);
                 let title = feedPost.childNodes[1].childNodes[0];
                 title.textContent = post.title;
                 let author = feedPost.childNodes[1].childNodes[1];
-                //console.log(author);
-                
                 author.textContent = "Posted by " + post.meta.author;
                 let upvotes = feedPost.firstChild;
                 upvotes.textContent = post.meta.upvotes.length;
                 let date = feedPost.childNodes[1].childNodes[2];
                 date.textContent = timeConverter(post.meta.published);
+                date.className = 'post-date';
                 let description = feedPost.childNodes[1].childNodes[3];
                 description.textContent = post.text;
                 let comments = feedPost.childNodes[1].childNodes[4];
@@ -325,8 +357,8 @@ function initApp(apiUrl) {
                 if (post.image !== null) {
                     let image = new Image();
                     image.src = 'data:image/png;base64,' + post.image;
-                    feedPost.appendChild(image);
                     image.className = 'post-image';
+                    feedPost.appendChild(image);
                 }
                 
                 feedUl.appendChild(feedPost);
@@ -335,6 +367,7 @@ function initApp(apiUrl) {
                 document.getElementById('root').appendChild(main);
                 
             }
+            // remove the fake template post
             feedLi.remove();
         }
     
@@ -347,7 +380,7 @@ function initApp(apiUrl) {
         let day = date.getDate();
         let hour = '0' + date.getHours();
         let min = '0' + date.getMinutes();
-        let time = 'at '+ hour.substr(-2) + ':' + min.substr(-2) + ', ' + day + ' ' + month + ' ' + year;
+        let time = ' at '+ hour.substr(-2) + ':' + min.substr(-2) + ', ' + day + ' ' + month + ' ' + year;
         return time;
     }
 }
